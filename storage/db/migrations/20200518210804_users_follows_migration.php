d0ac4b79a930c38456f5ec301c8d81459adaf8d6
arfenis <arfen00@gmail.com>
2020-05-18 17:08:38 -0400

    Add users follows to migration

---
+++ storage/db/migrations/20200518210804_users_follows_migration.php
@@ -0,0 +1,57 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class UsersFollowsMigration extends Phinx\Migration\AbstractMigration
+{
+    public function change()
+    {
+        $this->table('users_follows', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_general_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'identity' => 'enable',
+            ])
+            ->addColumn('users_id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'id',
+            ])
+            ->addColumn('entity_id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'users_id',
+            ])
+            ->addColumn('entity_namespace', 'string', [
+                'null' => false,
+                'limit' => 255,
+                'collation' => 'utf8mb4_general_ci',
+                'encoding' => 'utf8mb4',
+                'after' => 'entity_id',
+            ])
+            ->addColumn('is_deleted', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_TINY,
+                'after' => 'entity_namespace',
+            ])
+            ->addColumn('created_at', 'datetime', [
+                'null' => false,
+                'after' => 'is_deleted',
+            ])
+            ->addColumn('updated_at', 'datetime', [
+                'null' => true,
+                'default' => null,
+                'after' => 'created_at',
+            ])
+            ->create();
+    }
+}
