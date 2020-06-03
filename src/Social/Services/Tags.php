261bbd9fdd011698df7e6a14d11ad42bc25d2f55
arfenis <arfen00@gmail.com>
2020-06-03 12:57:35 -0400

    Create crud methods to tags service

--- src/Social/Services/Tags.php
+++ src/Social/Services/Tags.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Kanvas\Packages\Social\Services;
 
+use Kanvas\Packages\Social\Contract\Users\UserInterface;
+use Kanvas\Packages\Social\Models\Tags as TagsModel;
 use Kanvas\Packages\Social\Models\UserMessages;
+use Phalcon\Di;
+use Phalcon\Utils\Slug;
 
 class Tags
 {
@@ -15,7 +19,56 @@ class Tags
      * @param string $message
      * @return void
      */
-    public static function create(UserMessages $feed, string $message)
+    public static function process(UserMessages $feed, string $message)
     {
+    }
+
+    /**
+     * Create a new Tag
+     *
+     * @param UserInterface $user
+     * @param string $message
+     * @return TagsModel
+     */
+    public static function create(UserInterface $user, string $message): TagsModel
+    {
+        $newTag = new TagsModel();
+        $newTag->name = $message;
+        $newTag->apps_id = $user->getDefaultCompany()->getId();
+        $newTag->companies_id = Di::getDefault()->get('app')->getId();
+        $newTag->users_id = $user->id;
+        $newTag->slug = Slug::generate($message);
+        $newTag->saveOrFail();
+
+        return $newTag;
+    }
+
+    /**
+     * Update an existing Tag
+     *
+     * @param string $uuid
+     * @param string $message
+     * @return TagsModel
+     */
+    public static function update(string $uuid, string $message): TagsModel
+    {
+        $tag = TagsModel::findFirstOrFail($uuid);
+        $tag->name = $message;
+        $tag->slug = Slug::generate($message);
+        $tag->updateOrFail();
+
+        return $tag;
+    }
+
+    /**
+     * Delete an existing Tag
+     *
+     * @param UserInterface $user
+     * @return bool
+     */
+    public static function delete(string $uuid): bool
+    {
+        $tag = TagsModel::findFirstOrFail($uuid);
+        return $tag->deleteOrFail();
     }
 }
