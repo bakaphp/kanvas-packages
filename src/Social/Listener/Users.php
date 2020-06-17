6009fe5bfcf40fe52e26f58cf0f30c450b69f084
arfenis <arfen00@gmail.com>
2020-06-17 12:31:55 -0400

    Add doc to listener

--- src/Social/Listener/Users.php
+++ src/Social/Listener/Users.php
@@ -10,22 +10,61 @@ use Phalcon\Events\Event;
 
 class Users
 {
+    /**
+     * Create a save interaction between the user and the entity
+     *
+     * @param Event $event
+     * @param ModelInterface $entity
+     * @return void
+     */
     public function save(Event $event, ModelInterface $entity): void
     {
         Interactions::add(Di::getDefault()->get('userData'), $entity, InteractionsModel::SAVE);
     }
-    public function liked(Event $event, ModelInterface $entity): void
+
+    /**
+     * Create a like interaction between the user and the entity
+     *
+     * @param Event $event
+     * @param ModelInterface $entity
+     * @return void
+     */
+    public function like(Event $event, ModelInterface $entity): void
     {
         Interactions::add(Di::getDefault()->get('userData'), $entity, InteractionsModel::LIKE);
     }
+
+    /**
+     * Create a comment interaction between the user and the entity
+     *
+     * @param Event $event
+     * @param ModelInterface $entity
+     * @return void
+     */
     public function comment(Event $event, ModelInterface $entity): void
     {
         Interactions::add(Di::getDefault()->get('userData'), $entity, InteractionsModel::COMMENT);
     }
+
+    /**
+     * Create a reply interaction between the user and the entity
+     *
+     * @param Event $event
+     * @param ModelInterface $entity
+     * @return void
+     */
     public function reply(Event $event, ModelInterface $entity): void
     {
         Interactions::add(Di::getDefault()->get('userData'), $entity, InteractionsModel::REPLIED);
     }
+
+    /**
+     * Create a follow interaction between the user and the entity
+     *
+     * @param Event $event
+     * @param ModelInterface $entity
+     * @return void
+     */
     public function follow(Event $event, ModelInterface $entity): void
     {
         Interactions::add(Di::getDefault()->get('userData'), $entity, InteractionsModel::FOLLOWING);
