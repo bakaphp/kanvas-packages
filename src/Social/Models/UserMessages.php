e6405174033d7e37be5d3847d7ac9222f9094760
arfenis <arfen00@gmail.com>
2020-06-25 17:07:02 -0400

    Create method that fetch messages by user

--- src/Social/Models/UserMessages.php
+++ src/Social/Models/UserMessages.php
@@ -2,6 +2,9 @@
 
 namespace Kanvas\Packages\Social\Models;
 
+use Kanvas\Packages\Social\Contract\Users\UserInterface;
+use Phalcon\Mvc\Model\Resultset\Simple;
+
 class UserMessages extends BaseModel
 {
     public $messages_id;
@@ -15,5 +18,24 @@ class UserMessages extends BaseModel
     public function getSource()
     {
         return 'user_messages';
+    }
+
+    /**
+     * Return all the messages that the user have in its feed
+     *
+     * @param UserInterface $user
+     * @return Simple
+     */
+    public function getUserFeeds(UserInterface $user): Simple
+    {
+        $userFeeds = new Simple(
+            null,
+            new Messages(),
+            $this->getReadConnection()->query(
+                "SELECT * from messages where id in (SELECT messages_id from user_messages where users_id = {$user->getId()} and is_deleted = 0)"
+            )
+        );
+
+        return $userFeeds;
     }
 }
