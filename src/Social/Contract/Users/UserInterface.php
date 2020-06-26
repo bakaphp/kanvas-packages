b266f4689d8b79b58f5bbe21f3a1887d2e6e199e
arfenis <arfen00@gmail.com>
2020-06-26 17:34:37 -0400

    Remove unused

--- src/Social/Contract/Users/UserInterface.php
+++ src/Social/Contract/Users/UserInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Kanvas\Packages\Social\Contract\Users;
 
-use Phalcon\Mvc\ModelInterface;
-
 interface UserInterface
 {
     public function getDefaultCompany();
