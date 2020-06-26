e0017c548712b944bb02869b37fc48c687d38b64
arfenis <arfen00@gmail.com>
2020-06-26 17:33:36 -0400

    Add deleteInteraction in comment service

--- src/Social/Services/Comments.php
+++ src/Social/Services/Comments.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Kanvas\Packages\Social\Services;
 
+use Gewaer\Models\UsersInteractions;
+use Kanvas\Packages\Social\Contract\Users\UserInterface;
 use Kanvas\Packages\Social\Models\MessageComments;
 use Kanvas\Packages\Social\Models\Messages;
 
@@ -58,9 +60,10 @@ class Comments
      * @param string $commentId
      * @return boolean
      */
-    public static function delete(string $commentId): bool
+    public static function delete(string $commentId, UserInterface $user): bool
     {
         $comment = MessageComments::getByIdOrFail($commentId);
+        $comment->deleteInteraction(UsersInteractions::COMMENT, $user);
         return (bool) $comment->softDelete();
     }
 
