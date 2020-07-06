2235c87aacd3d4f2d029cd2cb408babd456ee7ae
arfenis <arfen00@gmail.com>
2020-07-06 17:09:48 -0400

    Create reaction model

---
+++ src/Social/Models/Reactions.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Kanvas\Packages\Social\Models;
+
+use Kanvas\Packages\Social\Contract\Interactions\CustomTotalInteractionsTrait;
+
+class Reactions extends BaseModel
+{
+    use CustomTotalInteractionsTrait;
+
+    public $id;
+    public $name;
+    public $apps_id;
+    public $companies_id;
+    public $icon;
+    public $is_deleted;
+    public $is_feature;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->hasMany(
+            'id',
+            UsersReactions::class,
+            'reactions_id',
+            [
+                'alias' => 'usersReactions'
+            ]
+        );
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'reactions';
+    }
+}
