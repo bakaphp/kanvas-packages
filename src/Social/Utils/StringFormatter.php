f59637ab04e740be9ad7a50291cba52b58f5ad8a
arfenis <arfen00@gmail.com>
2020-07-06 17:06:29 -0400

    Add methods to validate and get emojies from text

--- src/Social/Utils/StringFormatter.php
+++ src/Social/Utils/StringFormatter.php
@@ -10,6 +10,13 @@ namespace Kanvas\Packages\Social\Utils;
 class StringFormatter
 {
     /**
+     * String that contain regex to check emoji
+     *
+     * @var string
+     */
+    const EMOJIREGEX = '/([0-9#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u';
+
+    /**
      * Return an array of words that have hashtags
      *
      * @param string $text
@@ -20,5 +27,34 @@ class StringFormatter
         preg_match_all('/#(\w+)/', $text, $hashTag);
 
         return $hashTag[1];
+    }
+
+    /**
+     * Verify if the string if or have an emoji
+     *
+     * @param string $emoji
+     * @return boolean
+     */
+    public static function isStringEmoji(string $emoji): bool
+    {
+        return (bool) preg_match_all(
+            self::EMOJIREGEX,
+            $emoji
+        );
+    }
+
+    /**
+     * Return an array of emojis inside the text
+     *
+     * @param string $text
+     * @return array|bool
+     */
+    public static function getEmojiFromString(string $text)
+    {
+        if (preg_match_all(self::EMOJIREGEX, $text, $emojis)) {
+            return $emojis[0];
+        }
+
+        return false;
     }
 }
