ee71dfbc8719e1486ad25d6809c4966102fa6400
arfenis <arfen00@gmail.com>
2020-07-07 17:45:51 -0400

    Create job to eliminate all deleted reaction

---
+++ src/Social/Jobs/RemoveMessagesReactions.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Kanvas\Packages\Social\Jobs;
+
+use Canvas\Contracts\Queue\QueueableJobInterface;
+use Canvas\Jobs\Job;
+use Kanvas\Packages\Social\Models\Reactions as ReactionsModels;
+use Kanvas\Packages\Social\Models\UsersReactions;
+use Phalcon\Di;
+
+class RemoveMessagesReactions extends Job implements QueueableJobInterface
+{
+    protected $reaction;
+
+    /**
+     * Construct
+     *
+     * @param ReactionsModels $reaction
+     */
+    public function __construct(ReactionsModels $reaction)
+    {
+        $this->reaction = $reaction;
+    }
+
+    /**
+     * Handle that delete the reactions contains in any entity.
+     * Todo: Remove reaction count for the entity
+     * @return bool
+     */
+    public function handle(): bool
+    {
+        $messagesReaction = UsersReactions::find([
+            'conditions' => 'reactions_id = :reaction_id: AND is_deleted = 0',
+            'bind' => [
+                'reaction_id' => $this->reaction->getId()
+            ]
+        ]);
+
+        foreach ($messagesReaction as $messageReaction) {
+            $messageReaction->delete();
+        }
+
+        Di::getDefault()->get('log')->info('Delete message reactions from reaction id: ' . $this->message->getId());
+
+        return true;
+    }
+}
