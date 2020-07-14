2dd80922f9c417b3a19f130c33dc5c56932d6fbb
arfenis <arfen00@gmail.com>
2020-07-14 17:20:03 -0400

    Create comments services tests

--- tests/integration/Social/Services/CommentsCest.php
+++ tests/integration/Social/Services/CommentsCest.php
@@ -2,37 +2,98 @@
 
 namespace Kanvas\Packages\Tests\Integration\Social\Service;
 
+use Codeception\Lib\Di;
 use IntegrationTester;
-use Kanvas\Packages\Social\Services\Comments;
+use Kanvas\Packages\Social\Models\MessageComments;
 use Kanvas\Packages\Social\Models\Messages;
-use Kanvas\Packages\Tests\Support\Models\Users;
-use Phalcon\Di;
+use Kanvas\Packages\Social\Services\Comments;
+use Kanvas\Packages\Test\Support\Models\Users;
 
 class CommentsCest
 {
-    /**
-     * @var \IntegrationTester
-     */
-    // public $user;
-    
-    // protected function _before()
-    // {
-    //     $this->user = new Users();
-    //     dd($this->user);
-    //     $this->user = Di::getDefault()->setShared('userData', new Users());
-    // }
+    public $comment;
 
     /**
-     * Test to get HashtagFromStrings
+     * Get the first comment
+     *
+     * @return void
+     */
+    protected function getCommentData(): void
+    {
+        $this->comment = MessageComments::findFirst();
+    }
+    
+    /**
+     * Test add comment
      *
      * @param UnitTester $I
      * @return void
      */
     public function addComment(IntegrationTester $I): void
     {
-        $this->user = Di::getDefault()->setShared('userData', new Users());
-        dd($this->user);
-        $user = new Users();
-        dd($user->getId());
+        $feed = Messages::findFirst();
+        $comment = Comments::add($feed->getId(), 'test-text');
+
+        $I->assertEquals('test-text', $comment->message);
+    }
+
+    /**
+     * Test comment edit
+     *
+     * @param IntegrationTester $I
+     * @before getCommentData
+     * @return void
+     */
+    public function editComment(IntegrationTester $I): void
+    {
+        $comment = Comments::edit((string) $this->comment->getId(), 'edited-test-text');
+
+        $I->assertEquals('edited-test-text', $comment->message);
+    }
+
+    /**
+     * Test get Comment
+     *
+     * @param IntegrationTester $I
+     * @before getCommentData
+     * @return void
+     */
+    public function getComment(IntegrationTester $I): void
+    {
+        $comment = Comments::get((string) $this->comment->getId());
+
+        $I->assertNotNull($comment->getId());
+    }
+
+    /**
+     * Test reply comment
+     *
+     * @param IntegrationTester $I
+     * @before getCommentData
+     * @return void
+     */
+    public function replyComment(IntegrationTester $I): void
+    {
+        $reply = Comments::reply($this->comment->getId(), 'reply-test');
+
+        $I->assertEquals($reply->message, 'reply-test');
+        $I->assertEquals($reply->parent_id, $this->comment->getId());
+    }
+
+    /**
+     * Test edit comment
+     *
+     * @param IntegrationTester $I
+     * @before getCommentData
+     * @return void
+     */
+    public function deleteComment(IntegrationTester $I): void
+    {
+        $I->assertTrue(
+            Comments::delete(
+                (string) $this->comment->getId(),
+                new Users()
+            )
+        );
     }
 }
