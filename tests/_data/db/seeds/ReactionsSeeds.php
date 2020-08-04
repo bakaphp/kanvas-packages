e373fda69168e0b337ffcbb5a7048151b488d4a2
arfenis <arfen00@gmail.com>
2020-08-04 19:22:39 -0400

    Create reactions seeds for testing

---
+++ tests/_data/db/seeds/ReactionsSeeds.php
@@ -0,0 +1,41 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class ReactionsSeeds extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'confuse',
+                'icon' => "☹",
+                'apps_id' => 1,
+                'companies_id' => 1,
+                'is_deleted' => 0,
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'smile',
+                'icon' => '☺',
+                'apps_id'=> 1,
+                'companies_id' => 1,
+                'is_deleted' => 0,
+                'created_at' => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $posts = $this->table('reactions');
+        $posts->insert($data)
+              ->save();
+    }
+}
