a99bdd72d760ef652fcf30ff12ae4da1e83224f6
arfenis <arfen00@gmail.com>
2020-08-04 19:22:59 -0400

    Add reactions tests to messages entities

--- tests/integration/Social/Services/FeedsCest.php
+++ tests/integration/Social/Services/FeedsCest.php
@@ -6,11 +6,12 @@ use Codeception\Lib\Di;
 use IntegrationTester;
 use Kanvas\Packages\Social\Services\Feeds;
 use Kanvas\Packages\Social\Services\MessageTypes;
+use Kanvas\Packages\Social\Services\Reactions;
 use Kanvas\Packages\Test\Support\Models\Users;
 
 class FeedsCest
 {
-    public $comment;
+    public $message;
 
     /**
      * Get the first comment
@@ -21,12 +22,27 @@ class FeedsCest
     {
         MessageTypes::create(new Users(), 'memo', 'Test Type');
     }
+
+    /**
+     * Create a message for testing
+     *
+     * @return void
+     */
+    protected function messageTestCreation(): void
+    {
+        {
+            $text = [
+                'text' => 'Test some messages'
+            ];
+            
+            $this->message = Feeds::create(new Users(), 'memo', $text);
+        }
+    }
         
     /**
      * Test add comment
      *
      * @param UnitTester $I
-     * @before createMessageType
      * @return void
      */
     public function createMessage(IntegrationTester $I): void
@@ -38,5 +54,21 @@ class FeedsCest
         $feed = Feeds::create(new Users(), 'memo', $text);
 
         $I->assertNotNull($feed->getId());
+    }
+
+    /**
+     * Test Message Reactions
+     *
+     * @param IntegrationTester $I
+     * @before messageTestCreation
+     * @return void
+     */
+    public function messageReaction(IntegrationTester $I): void
+    {
+        $reaction = Reactions::addMessageReaction('confuse', new Users(), $this->message);
+        $I->assertEquals($this->message->getId(), $reaction->entity_id);
+
+        $reaction = Reactions::addMessageReaction('☺', new Users(), $this->message);
+        $I->assertEquals($this->message->getId(), $reaction->entity_id);
     }
 }
