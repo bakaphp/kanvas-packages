f96e42a53d83ac6075879ddf8830b1417c3c655a
arfenis <arfen00@gmail.com>
2020-08-31 12:28:29 -0400

    Create channels seeds for testing

---
+++ tests/_data/db/seeds/ChannelsSeeds.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class ChannelsSeeds extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'profile',
+                'description' => 'Basic Profile',
+                'created_at' => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $posts = $this->table('channels');
+        $posts->insert($data)
+              ->save();
+    }
+}
