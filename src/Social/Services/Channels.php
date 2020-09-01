8e4537f322d21afd29659f90c89c8bebe8d8f3f9
arfenis <arfen00@gmail.com>
2020-09-01 16:57:40 -0400

    Create method to get channel by name and add slug

--- src/Social/Services/Channels.php
+++ src/Social/Services/Channels.php
@@ -4,12 +4,33 @@ declare(strict_types=1);
 
 namespace Kanvas\Packages\Social\Services;
 
+use Kanvas\Packages\Social\Contract\Channels\ChannelsInterface;
 use Kanvas\Packages\Social\Contract\Users\UserInterface;
 use Kanvas\Packages\Social\Models\Channels as ChannelsModel;
 use Kanvas\Packages\Social\Models\ChannelUsers;
+use Phalcon\Utils\Slug;
 
 class Channels
 {
+
+    /**
+     * Get Channel by its name
+     *
+     * @param string $channelName
+     * @return ChannelsModel
+     */
+    public static function getChannelByName(string $channelName): ChannelsModel
+    {
+        $channel = ChannelsModel::findFirstOrFail([
+            'conditions' => 'slug = :slug: AND is_deleted = 0',
+            'bind' => [
+                'slug' => Slug::generate($channelName)
+            ]
+        ]);
+
+        return $channel;
+    }
+
     /**
      * Create a new Channel and assign his creator a rol
      *
@@ -18,11 +39,14 @@ class Channels
      * @param string $description
      * @return ChannelsModel
      */
-    public static function create(UserInterface $user, string $name, string $description = ''): ChannelsModel
+    public static function create(UserInterface $user, ChannelsInterface $channel_entity, string $name, string $description = ''): ChannelsModel
     {
         $channel = new ChannelsModel();
         $channel->name = $name;
+        $channel->slug = Slug::generate($name);
         $channel->description = $description;
+        $channel->entity_namespace = get_class($channel_entity);
+        $channel->entity_id = (string) $channel_entity->getId();
         $channel->saveOrFail();
 
         self::addUser($channel->getId(), $user);
