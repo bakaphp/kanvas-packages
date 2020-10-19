85b103d2113693f1520bd47e2ae6d32f8ad96712
Rafael White <rwhite@mctekk.com>
2020-10-19 14:27:26 -0400

    Add workflow rules folder and within it the models.

---
+++ src/WorkflowRules/Models/Rules.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Kanvas\Packages\WorkflowsRules\Models;
+
+class Rules extends BaseModel
+{
+    public int $system_modules_id;
+    public int $rules_types_id;
+    public string $name;
+    public string $description;
+    public string $pattern;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSource('rules');
+
+        $this->hasMany(
+            'id',
+            RulesConditions::class,
+            'rules_id',
+            ['alias' => 'rulesConditions']
+        );
+
+        $this->hasMany(
+            'id',
+            RulesActions::class,
+            'rules_id',
+            ['alias' => 'rulesActions']
+        );
+
+        $this->belongsTo(
+            'rules_types_id',
+            RulesTypes::class,
+            'id',
+            ['alias' => 'rulesTypes']
+        );
+    }
+}
