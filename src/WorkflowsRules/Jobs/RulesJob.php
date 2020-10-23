ef6eecf96bcfd9521c93fd37f254655a8cd271c0
FredPeal <frederickpeal@gmail.com>
2020-10-23 17:32:25 -0400

    services validate test

--- src/WorkflowsRules/Jobs/RulesJob.php
+++ src/WorkflowsRules/Jobs/RulesJob.php
@@ -4,19 +4,39 @@ namespace Kanvas\Packages\WorkflowsRules\Jobs;
 
 use Baka\Jobs\Job;
 use Kanvas\Packages\WorkflowsRules\Models\Rules;
+use Kanvas\Packages\WorkflowsRules\Services\Rules as RulesServices;
 
 class RulesJob extends Job
 {
     public Rules $rule;
     public string $event;
+    public object $entity;
 
+    /**
+     * __construct.
+     *
+     * @param  Rules $rules
+     * @param  string $event
+     * @param  object $entity
+     *
+     * @return void
+     */
     public function __construct(Rules $rules, string $event, object $entity)
     {
         $this->rule = $rules;
         $this->onQueue('workflows');
+        $this->entity = $entity;
     }
 
+    /**
+     * handle.
+     *
+     * @return void
+     */
     public function handle()
     {
+        dump(get_class($this->rule));
+        $rule = RulesServices::set($this->rule);
+        $rule->validate($this->entity);
     }
 }
--- src/WorkflowsRules/Services/Rules.php
+++ src/WorkflowsRules/Services/Rules.php
@@ -1,18 +1,65 @@
 <?php
 
-namespace Kanvas\Packages\WorkflowRules\Services;
+namespace Kanvas\Packages\WorkflowsRules\Services;
 
-use Kanvas\Packages\WorkflowRules\Models\Rules as RulesModel;
+use Kanvas\Packages\WorkflowsRules\Models\Rules as RulesModel;
+use Phalcon\Di;
 
 class Rules
 {
     public RulesModel $rule;
     protected string $condition;
 
-    public function set(RulesModel $rules)
+    /**
+     * set.
+     *
+     * @param  RulesModel $rules
+     *
+     * @return void
+     */
+    public static function set(RulesModel $rules)
+    {
+        return (new static())->assignRule($rules);
+    }
+
+    /**
+     * validate.
+     *
+     * @param  bool $entity
+     *
+     * @return bool
+     */
+    public function validate(object $entity) : bool
+    {
+        Di::getDefault()->get('log')->info('Rule validate');
+        return true;
+    }
+
+    /**
+     * assignPattern.
+     *
+     * @param  string $pattern
+     *
+     * @return void
+     */
+    private function assignPattern(string $pattern) : Rules
+    {
+        $this->condition = $pattern;
+        return $this;
+    }
+
+    /**
+     * assignRule.
+     *
+     * @param  RulesModel $rules
+     *
+     * @return void
+     */
+    private function assignRule(RulesModel $rules) : Rules
     {
         $this->rule = $rules;
-        $this->condition = $this->rule->pattern;
+        $this->assignPattern($rules->pattern);
+        return $this;
     }
 
     /**
@@ -22,16 +69,22 @@ class Rules
      *
      * @return void
      */
-    public function setByName(string $name) : void
+    public static function setByName(string $name) : self
     {
         $rules = RulesModel::findFirstOrFail([
             'conditions' => 'name = :name:',
             'bind' => ['name' => $name]
         ]);
 
-        $this->rule = $rules;
+        self::$rules = $rules;
+        return new static();
     }
 
+    /**
+     * getStringConditions.
+     *
+     * @return string
+     */
     public function getStringConditions() : string
     {
         $conditions = $this->rules->getRulesConditions();
