d5e280f2cec1f0673cd927ed769c01503966c0ee
FredPeal <frederickpeal@gmail.com>
2020-11-17 23:18:24 -0400

    Get system modules from class

--- src/WorkflowsRules/Contracts/Traits/RulesTrait.php
+++ src/WorkflowsRules/Contracts/Traits/RulesTrait.php
@@ -2,9 +2,11 @@
 
 namespace Kanvas\Packages\WorkflowsRules\Contracts\Traits;
 
+use Baka\Database\SystemModules;
 use Kanvas\Packages\WorkflowsRules\Jobs\RulesJob;
 use Kanvas\Packages\WorkflowsRules\Models\Rules;
 use Kanvas\Packages\WorkflowsRules\Models\RulesTypes;
+use Phalcon\Di;
 
 trait RulesTrait
 {
@@ -19,13 +21,14 @@ trait RulesTrait
      */
     public function fireRules(string $event) : void
     {
-        if ($this->systems_modules) {
+        $systemModules = $this->getSystemModules();
+        if ($systemModules) {
             $rulesTypes = RulesTypes::findFirstByName($event);
 
             $rules = Rules::find([
                 'conditions' => 'systems_modules_id = :systems_module_id: AND rules_types_id = :rules_types_id: AND companies_id = :companies_id:',
                 'bind' => [
-                    'systems_module_id' => $this->systems_modules->getId(),
+                    'systems_module_id' => $systemModules->getId(),
                     'rules_types_id' => $rulesTypes->getId(),
                     'companies_id' => $this->companies->getId()
                 ]
@@ -35,5 +38,25 @@ trait RulesTrait
                 RulesJob::dispatch($rule, $event, $this);
             }
         }
+    }
+
+    /**
+     * getSystemModules.
+     *
+     * @return SystemModules|null
+     */
+    public function getSystemModules() : ?SystemModules
+    {
+        $di = Di::getDefault();
+        $appId = $di->get('app')->getId();
+
+        $systemModules = SystemModules::findFirst([
+            'conditions' => 'model_name = :model_name: AND apps_id = :apps_id:',
+            'bind' => [
+                'model_name' => get_class($this),
+                'apps_id' => $appId
+            ]
+        ]);
+        return $systemModules;
     }
 }
