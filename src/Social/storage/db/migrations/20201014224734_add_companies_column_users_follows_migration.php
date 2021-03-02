9ec9b25ab821f93d971e553bcb34d8015908fd69
Rafael White <rwhite@mctekk.com>
2021-03-02 11:33:29 -0400

    Fix migration issue

--- src/Social/storage/db/migrations/20201014224734_add_companies_column_users_follows_migration.php
+++ src/Social/storage/db/migrations/20201014224734_add_companies_column_users_follows_migration.php
@@ -7,26 +7,22 @@ class AddCompaniesColumnUsersFollowsMigration extends Phinx\Migration\AbstractMi
     public function change()
     {
         $this->table('users_follows', [
-                'id' => false,
-                'primary_key' => ['id'],
-                'engine' => 'InnoDB',
-                'encoding' => 'utf8mb4',
-                'collation' => 'utf8mb4_general_ci',
-                'comment' => '',
-                'row_format' => 'DYNAMIC',
-            ])
+            'id' => false,
+            'primary_key' => ['id'],
+            'engine' => 'InnoDB',
+            'encoding' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci',
+            'comment' => '',
+            'row_format' => 'DYNAMIC',
+        ])
             ->addColumn('companies_id', 'integer', [
                 'null' => true,
-                'limit' => 11,
-                'collation' => 'utf8mb4_general_ci',
-                'encoding' => 'utf8mb4',
+                'limit' => MysqlAdapter::INT_REGULAR,
                 'after' => 'entity_id',
             ])
             ->addColumn('companies_branches_id', 'integer', [
                 'null' => true,
-                'limit' => 11,
-                'collation' => 'utf8mb4_general_ci',
-                'encoding' => 'utf8mb4',
+                'limit' => MysqlAdapter::INT_REGULAR,
                 'after' => 'companies_id',
             ])
             ->save();
