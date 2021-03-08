df8d0d5f6e4aa2b500095c27ef58f33608bfa052
Rafael White <rwhite@mctekk.com>
2021-03-08 15:07:46 -0400

    Add config and app providers

--- tests/providers.php
+++ tests/providers.php
@@ -8,10 +8,14 @@ use Kanvas\Packages\Payments\Providers\PlaidProvider;
 use Kanvas\Packages\Social\Providers\DatabaseProvider;
 use Kanvas\Packages\Social\Providers\QueueProvider;
 use Kanvas\Packages\Social\Providers\RedisProvider;
-use  Kanvas\Packages\WorkflowsRules\Providers\DatabaseProvider as WorkflowDatabaseProvider;
+use Kanvas\Packages\WorkflowsRules\Providers\DatabaseProvider as WorkflowDatabaseProvider;
 use Kanvas\Packages\Social\Providers\ModelsCacheProvider;
+use Canvas\Providers\ConfigProvider;
+use Canvas\Providers\AppProvider;
 
 return [
+    ConfigProvider::class,
+    AppProvider::class,
     DatabaseProvider::class,
     QueueProvider::class,
     RedisProvider::class,
