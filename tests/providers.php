d5b5bf88cf0c595f75fc3b23c2a05f9f448c0497
Rafael White <rwhite@mctekk.com>
2021-03-08 16:57:58 -0400

    Roll back changes in tests providers

--- tests/providers.php
+++ tests/providers.php
@@ -9,17 +9,11 @@ use Kanvas\Packages\Social\Providers\DatabaseProvider;
 use Kanvas\Packages\Social\Providers\QueueProvider;
 use Kanvas\Packages\Social\Providers\RedisProvider;
 use Kanvas\Packages\WorkflowsRules\Providers\DatabaseProvider as WorkflowDatabaseProvider;
-use Kanvas\Packages\Social\Providers\ModelsCacheProvider;
-use Kanvas\Packages\Social\Providers\ConfigProvider;
-use Canvas\Providers\AppProvider;
 
 return [
-    // ConfigProvider::class,
-    // AppProvider::class,
     DatabaseProvider::class,
     QueueProvider::class,
     RedisProvider::class,
-    // ModelsCacheProvider::class,
     PlaidProvider::class,
     WorkflowDatabaseProvider::class
 ];
