ec97d7922339af86c42c29d89ad953bd83de55b4
Rafael White <rwhite@mctekk.com>
2021-03-09 10:03:33 -0400

    Add exception dependency namespace

--- src/Social/Models/SystemModules.php
+++ src/Social/Models/SystemModules.php
@@ -4,6 +4,7 @@ namespace Kanvas\Packages\Social\Models;
 
 use Canvas\Models\SystemModules as KanvasSystemModules;
 use Phalcon\Mvc\ModelInterface;
+use Baka\Http\Exception\InternalServerErrorException;
 use Phalcon\Di;
 
 class SystemModules extends KanvasSystemModules
