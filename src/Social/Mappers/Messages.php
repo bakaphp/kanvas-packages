e333079de57466e6c4673b4e51e6b10ecb56756d
kaioken <max@mctekk.com>
2021-03-16 02:23:13 -0400

    parse custom fields

--- src/Social/Mappers/Messages.php
+++ src/Social/Mappers/Messages.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Kanvas\Packages\Social\Mappers;
 
 use AutoMapperPlus\CustomMapper\CustomMapper;
+use function Baka\isJson;
+use Kanvas\Packages\Social\Dto\Messages as DtoMessages;
+use Kanvas\Packages\Social\ElasticDocuments\Messages as ElasticDocumentsMessages;
 
 class Messages extends CustomMapper
 {
@@ -12,11 +15,28 @@ class Messages extends CustomMapper
      * @param ElasticDocumentsMessages $message
      * @param DtoMessages $messageDto
      *
-     * @return DtoMessages
+     * @return ElasticDocumentsMessages
      */
     public function mapToObject($message, $messageDto, array $context = [])
     {
         //no need to convert , we will interact directly with the elastic document
+        $message->custom_fields = $message->custom_fields ? $this->formatCustomFields($message->custom_fields) : [];
         return $message;
+    }
+
+    /**
+     * Format json custom fields.
+     *
+     * @param array $customFields
+     *
+     * @return array
+     */
+    protected function formatCustomFields(array $customFields) : array
+    {
+        foreach ($customFields as $key => $customField) {
+            $customFields[$key] = is_string($customField) && isJson($customField) ? json_decode($customField, true) : $customField;
+        }
+
+        return $customFields;
     }
 }
