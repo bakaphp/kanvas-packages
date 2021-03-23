101acbdab6c1127c60a27f7e8c03f591de0f89a0
Rafael White <rwhite@mctekk.com>
2021-03-23 15:26:54 -0400

    Add data type to comments dto

--- src/Social/Dto/Comments.php
+++ src/Social/Dto/Comments.php
@@ -4,15 +4,15 @@ namespace Kanvas\Packages\Social\Dto;
 
 class Comments
 {
-    public $id;
-    public $apps_id;
-    public $companies_id;
+    public int $id;
+    public int $apps_id;
+    public int $companies_id;
     public $users;
-    public $message_id;
+    public int $message_id;
     public $message;
-    public $reactions_count;
-    public $parent_id;
-    public $created_at;
-    public $updated_at;
-    public $is_deleted;
+    public int $reactions_count;
+    public int $parent_id;
+    public string $created_at;
+    public ?string $updated_at = null;
+    public int $is_deleted = 0;
 }
