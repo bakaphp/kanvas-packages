8118c6b9debb0e06a7d7e34a28537bd8dd275dc6
kaioken <max@mctekk.com>
2021-08-30 21:23:58 -0400

    refact : recommendation contract

--- src/Recommendations/Contracts/Recommendation.php
+++ src/Recommendations/Contracts/Recommendation.php
@@ -3,6 +3,16 @@ declare(strict_types=1);
 
 namespace Kanvas\Packages\Recommendation\Contracts;
 
+use Baka\Contracts\Auth\UserInterface;
+use Baka\Contracts\ModelInterface;
+
 interface Recommendation
 {
+    public function itemsToUser(UserInterface $user, int $total, array $options) : ?array;
+    public function itemsToItems(
+        ModelInterface $model,
+        UserInterface $user,
+        int $total,
+        array $options
+    ) : ?array;
 }
