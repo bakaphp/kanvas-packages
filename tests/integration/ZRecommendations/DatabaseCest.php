6308d1ff2e36a196039346de352f8862614917d1
kaioken <max@mctekk.com>
2021-09-05 02:30:21 -0400

    fix : test order

--- tests/integration/ZRecommendations/DatabaseCest.php
+++ tests/integration/ZRecommendations/DatabaseCest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Kanvas\Packages\Tests\Integration\Payments\Plaid;
+namespace Kanvas\Packages\Tests\Integration\ZRecommendations;
 
 use IntegrationTester;
 use Kanvas\Packages\Recommendations\Drivers\Recombee\Engine;
