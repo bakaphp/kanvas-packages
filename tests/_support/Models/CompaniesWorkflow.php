2c133a3d523660da3ac0db91158d33a6f788ad2e
kaioken <max@mctekk.com>
2021-09-11 23:38:40 -0400

    refact : contracts

--- tests/_support/Models/CompaniesWorkflow.php
+++ tests/_support/Models/CompaniesWorkflow.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Kanvas\Packages\Test\Support\Models;
 
 use Canvas\Models\Companies as KanvasCompanies;
-use Kanvas\Packages\WorkflowsRules\Contracts\Interfaces\WorkflowsEntityInterfaces;
+use Kanvas\Packages\WorkflowsRules\Contracts\WorkflowsEntityInterfaces;
 use Kanvas\Packages\WorkflowsRules\Traits\CanUseRules;
 
 class CompaniesWorkflow extends KanvasCompanies implements WorkflowsEntityInterfaces
